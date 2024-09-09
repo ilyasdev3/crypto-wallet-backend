2f91a16aab0e5480e56152bf58a4304e36a0dd78
ilyasdev3 <ilyassanjrani708@gmail.com>
2024-09-09 22:18:43 +0500

    Update user model

--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -1,22 +1,19 @@
-import { UserDocument, UserModel, UserSchema } from './../types/mongoose.gen';
+import { UserDocument, UserModel, UserSchema } from "./../types/mongoose.gen";
 import { Schema, model } from "mongoose";
 
-
-
 const userSchema: UserSchema = new Schema({
-  firstName: { type: String,  },
-  lastName: { type: String,  },
-  username: { type: String,  unique: true },
+  firstName: { type: String },
+  lastName: { type: String },
+  username: { type: String, unique: true },
   password: { type: String, required: true },
-  email: { type: String, },
-  phone: { type: String, },
-  address: { type: String,  },
-  isVerified: { type: Boolean, default: false },
+  email: { type: String },
+  phone: { type: String },
+  address: { type: String },
+  isActive: { type: Boolean, default: true },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
 });
 
- const UserModel:UserModel = model<UserDocument, UserModel>("User", userSchema);
+const UserModel: UserModel = model<UserDocument, UserModel>("User", userSchema);
 
- export {UserModel}
-
+export { UserModel };
