222952e738fb10075c2bfa69eee95c2005a8f1ba
ilyasdev3 <ilyassanjrani708@gmail.com>
2024-09-09 22:22:55 +0500

    Update cloudinary

--- src/utils/uploadToCloudinary.ts
+++ src/utils/uploadToCloudinary.ts
@@ -4,7 +4,7 @@ import { v2 as cloudinary } from "cloudinary";
 cloudinary.config({
   cloud_name: "doo90wesa",
   api_key: "375892526598345",
-  api_secret: "4zQok0eKHSH1epdarsu7RFT2aQI", // Click 'View API Keys' above to copy your API secret
+  api_secret: process.env.CLOUDINARY_SECRET,
 });
 
 // Upload an image
