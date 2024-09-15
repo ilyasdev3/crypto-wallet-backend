54e4c440c95dcc01d83a9afb12bbb5b2e22501c5
ilyasdev3 <ilyassanjrani708@gmail.com>
2024-09-15 21:28:43 +0500

    Add comment model

--- src/models/Comment.model.ts
+++ src/models/Comment.model.ts
@@ -2,7 +2,7 @@ import { Schema, model } from "mongoose";
 import { CommentDocument, CommentModel } from "../types/mongoose.gen";
 
 const commentSchema = new Schema({
-  userId: { type: Schema.Types.ObjectId, ref: "User", required: true }, // Reference to User model
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true }, // Reference to User model
   postId: { type: Schema.Types.ObjectId, ref: "Post", required: true }, // Reference to Post model
   content: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
