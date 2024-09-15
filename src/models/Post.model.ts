7b3a01f648729703fcbb481139d58117a0780bcf
ilyasdev3 <ilyassanjrani708@gmail.com>
2024-09-15 08:02:29 +0500

    Add post model

--- src/models/Post.model.ts
+++ src/models/Post.model.ts
@@ -1,15 +1,20 @@
+import { PostDocument, PostModel } from "./../types/mongoose.gen";
 import { Schema, model } from "mongoose";
 
 const postSchema = new Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
   isVerified: { type: Boolean, required: true, default: false },
-  community: { type: String, required: true },
-  user: { type: String, required: true },
+  communityId: {
+    type: Schema.Types.ObjectId,
+    ref: "Community",
+  },
+  file: { type: String },
+  userId: { type: Schema.Types.ObjectId, ref: "User", required: true }, // Reference to User model
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
 });
 
-const Post = model("Post", postSchema);
+const PostModel: PostModel = model<PostDocument, PostModel>("Post", postSchema);
 
-export default Post;
+export { PostModel };
