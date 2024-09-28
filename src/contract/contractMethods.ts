3072a5be76c29c26a4831e291ede55855bf65d64
ilyasdev3 <ilyassanjrani708@gmail.com>
2024-09-28 21:24:08 +0500

    Update contract

--- src/contract/contractMethods.ts
+++ src/contract/contractMethods.ts
@@ -1,32 +1,66 @@
 import { ethers } from "ethers";
-import contractABI from "./contractABI.json";
+import  contractABI  from "./contractABI.json";
 
-const providerUrl =
-  "https://eth-holesky.g.alchemy.com/v2/3l1FowDVfzUhd5gaW7-Z_9f_lTOhgXCU";
 
-export const contractAddress = "0x1B5A0792A1B712d855eBa0AE8b477F8F02a549E1";
-const provider = new ethers.JsonRpcProvider(providerUrl);
+// Define the provider with network details
+const provider = new ethers.providers.JsonRpcProvider(
+  "https://rpc.holesky.ethpandaops.io"
+);
 
+
+// Sample function to get wallet balance
 export const getWalletBalance = async (
-  contractAddress: any,
-  privateKey: any
+  contractAddress: string,
+  privateKey: string
 ) => {
-  const wallet = new ethers.Wallet(privateKey, provider);
-  console.log("wallet inside getWalletBalance", wallet);
+  try {
+    const wallet = new ethers.Wallet(privateKey, provider);
+    console.log("Wallet inside getWalletBalance:", wallet.address);
 
-  const contract = new ethers.Contract(contractAddress, contractABI, wallet);
-  const balance = await contract.balanceOf(wallet.address);
-  console.log("balance", balance);
-  return ethers.formatEther(balance);
+    // Ensure contract initialization with wallet
+    const contractWithWallet = new ethers.Contract(
+      contractAddress,
+      contractABI,
+      wallet
+    );
+    
+
+    // Fetch the balance
+    const balance = await contractWithWallet.balanceOf(wallet.address);
+    console.log("Balance:", ethers.utils.formatEther(balance));
+
+    return ethers.utils.formatEther(balance);
+  } catch (error) {
+    console.error("Error in getWalletBalance:", error);
+    return null; // Return null or throw error based on requirement
+  }
 };
+
 
 export const transfer = async (
   contractAddress: any,
   amount: any,
-  privateKey: any
+  privateKey: any,
+  address: any
 ) => {
+
+  console.log("privateKey in transfer:", privateKey);
+  console.log("address in transfer:", address);
+  console.log("amount in transfer:", amount);
+  
   const wallet = new ethers.Wallet(privateKey, provider);
   const contract = new ethers.Contract(contractAddress, contractABI, wallet);
-  const tx = await contract.transfer(wallet.address, ethers.parseEther(amount));
-  await tx.wait();
+  const tx = await contract.transfer(address, ethers.utils.parseEther(amount));
+  console.log("tx in transfer:", tx);
+  
+  // await tx.wait();
+
+  const getBalance = await getWalletBalance(contractAddress, privateKey);
+  console.log("getBalance in transfer:", getBalance);
+  
+  
+  return tx;
 };
+
+
+
