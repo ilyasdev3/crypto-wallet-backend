f88171c887c5bc63f4986186b6f61bf8b2298a10
ilyasdev3 <ilyassanjrani708@gmail.com>
2024-09-28 21:21:52 +0500

    Create model for contract

---
+++ src/models/Contract.model.ts
@@ -0,0 +1,20 @@
+import { ContractDocument, ContractModel } from './../types/mongoose.gen';
+import { Schema, model } from "mongoose";
+
+
+const contractSchema = new Schema({
+  name: { type: String, required: true }, 
+  contractAddress: { type: String, required: true, unique: true },
+  provider: { type: String, required: true }, 
+  abi: { type: Array, required: false },
+  network: { type: String, required: true },
+  userId: { type: Schema.Types.ObjectId, ref: 'User', required: true }, 
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+
+const ContractModel = model<ContractDocument, ContractModel>("Contract", contractSchema);
+
+export {ContractModel}
+
