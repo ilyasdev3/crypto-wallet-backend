7d5464a3d15a716704be8bd722ba5f708c74e802
ilyasdev3 <ilyassanjrani708@gmail.com>
2024-11-30 23:24:16 +0500

    Update transaction model

---
+++ src/models/Transaction.model.ts
@@ -0,0 +1,32 @@
+import { Schema, model } from "mongoose";
+import {
+  TransactionDocument,
+  TransactionModel,
+  TransactionSchema,
+} from "./../types/mongoose.gen";
+
+const transactionSchema: TransactionSchema = new Schema({
+  senderWalletId: {
+    type: Schema.Types.ObjectId,
+    ref: "Wallet",
+    required: true,
+  },
+  receiverWalletId: {
+    type: Schema.Types.ObjectId,
+    ref: "Wallet",
+    required: true,
+  },
+  contractId: { type: Schema.Types.ObjectId, ref: "Contract", required: true },
+  transactionHash: { type: String, required: true },
+  amount: { type: String, required: true },
+  status: { type: String, required: true, default: "pending" },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+const TransactionModel = model<TransactionDocument, TransactionModel>(
+  "Transaction",
+  transactionSchema
+);
+
+export { TransactionModel };
