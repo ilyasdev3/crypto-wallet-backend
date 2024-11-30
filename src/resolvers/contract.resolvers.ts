f2cdd2d2c66920d2d13cd8fccbe0b5a66312aa57
ilyasdev3 <ilyassanjrani708@gmail.com>
2024-11-30 23:24:45 +0500

    Update contract resolver

--- src/resolvers/contract.resolvers.ts
+++ src/resolvers/contract.resolvers.ts
@@ -1,7 +1,7 @@
-import { MutationResolvers } from './../types/types.generated';
+import { MutationResolvers } from "./../types/types.generated";
 import { UserModel } from "../models/User.model";
 import { WalletModel } from "../models/Wallet.model";
-import {ContractModel} from "../models/Contract.model";
+import { ContractModel } from "../models/Contract.model";
 
 import { IContext } from "../types/context.types";
 import axios from "axios";
@@ -10,17 +10,17 @@ import { QueryResolvers } from "../types/types.generated";
 // export const contractAddress = "0x080c78d90209bb9bFA0bACff759761cC1FBf35ff";
 export const contractAddress = "0x57467aA72c2980CeC0455A489Fe4B1eeB1De4A21";
 
-export const walletQueries: QueryResolvers<IContext> = {
-getContracts: async (parent, __, { error }) => {
+export const contractQueries: QueryResolvers<IContext> = {
+  getContracts: async (parent, __, { error }) => {
     if (error) throw error;
 
     const contracts = await ContractModel.find({});
     console.log("contracts", contracts);
     return contracts as any;
   },
-}
+};
 
-export const walletMutations: MutationResolvers<IContext> = {
+export const contractMutations: MutationResolvers = {
   createContract: async (parent, { contract }, { error, user }) => {
     console.log("user at top", user);
     console.log("contract at top", contract);
@@ -31,18 +31,22 @@ export const walletMutations: MutationResolvers<IContext> = {
     const { name, contractAddress, provider, abi, network, userId } = contract;
 
     if (!name || !contractAddress || !provider || !abi || !network || !userId)
-      throw new Error("Name, contractAddress, provider, abi, network, and userId are required");
+      throw new Error(
+        "Name, contractAddress, provider, abi, network, and userId are required"
+      );
 
     const existingContract = await ContractModel.findOne({ contractAddress });
     console.log("existingContract", existingContract);
 
     if (existingContract) throw new Error("Contract already exists");
 
+    const stringAbi = JSON.stringify(abi);
+
     const newContract = await new ContractModel({
       name,
       contractAddress,
       provider,
-      abi,
+      abi: stringAbi,
       network,
       userId,
     }).save();
