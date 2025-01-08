8d6489b45fea48d840088f346d4e5579b14e2298
ilyasdev3 <ilyassanjrani708@gmail.com>
2025-01-08 20:49:14 +0500

    Update transaction resolver

--- src/resolvers/transaction.resolvers.ts
+++ src/resolvers/transaction.resolvers.ts
@@ -1,63 +1,27 @@
 import { UserModel } from "../models/User.model";
 import { WalletModel } from "../models/Wallet.model";
 import { TransactionModel } from "../models/Transaction.model";
-
 import { QueryResolvers, MutationResolvers } from "../types/types.generated";
 import { IContext } from "../types/context.types";
 import { GraphQLError } from "graphql";
+import { PipelineStage } from "mongoose";
 
 export const transactionQueries: QueryResolvers<IContext> = {
-  getUserTransactions: async (parent, _, { user, error }) => {
+  getUserTransactions: async (parent, { input }, { user, error }) => {
     if (error) throw error;
-    if (!user) throw new Error("User not found");
+    if (!user) throw new GraphQLError("User not found");
 
-    console.log("user", user);
+    try {
+      const transactions = await TransactionModel.find({
+        ownerId: user.id,
+        status: input.type,
+      });
 
-    const userWallet = await WalletModel.findOne({ userId: user.id });
-    if (!userWallet) throw new Error("Wallet not found");
-    console.log("userWallet", userWallet);
-
-    // Query transactions involving the user's wallet
-    const transactions = await TransactionModel.aggregate([
-      {
-        $match: {
-          $or: [
-            { senderWalletId: userWallet._id },
-            { receiverWalletId: userWallet._id },
-          ],
-        },
-      },
-      {
-        $addFields: {
-          priority: {
-            $cond: {
-              if: { $eq: ["$receiverWalletId", userWallet._id] },
-              then: 1, // Higher priority for receiving transactions
-              else: 0, // Lower priority for sending transactions
-            },
-          },
-        },
-      },
-      {
-        $sort: {
-          priority: -1, // Prioritize receiving transactions for receiver
-          createdAt: -1, // Order by the most recent transaction within priority
-        },
-      },
-      {
-        $group: {
-          _id: "$transactionHash", // Group by transaction hash
-          transaction: { $first: "$$ROOT" }, // Pick the first document in each group
-        },
-      },
-      {
-        $replaceRoot: { newRoot: "$transaction" }, // Flatten the grouped result
-      },
-    ]);
-
-    console.log("transactions", transactions);
-
-    return transactions as any;
+      return transactions as any;
+    } catch (error) {
+      console.error("Error fetching transactions:", error);
+      throw new GraphQLError("Failed to fetch transactions");
+    }
   },
 };
 
